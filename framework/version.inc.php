7b3217efeb0d4e55d012e1b70d8f8299de0daf79
chaiwenjun <easy24@126.com>
2019-01-10 18:23:19 +0800

    add
    微擎1.73简化去版权
    
    Co-Authored-By: chaiwenjun <chaiwenjun@users.noreply.github.com>

---
+++ framework/version.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 版本号
+ *
+ * [WeEngine System] Copyright (c) 2013 qdyma.com
+ */
+
+defined('IN_IA') or exit('Access Denied');
+
+define('IMS_FAMILY', 'v');
+define('IMS_VERSION', '1.7.3');
+define('IMS_RELEASE_DATE', '201804280001');
